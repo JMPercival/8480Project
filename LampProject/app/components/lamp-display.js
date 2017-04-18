6a2d23eb35f3376e9cab342a359b1df53416246e
James Percival <jpercival@unomaha.edu>
2017-04-18 14:14:55 -0500

    on/off pushed and tested

--- LampProject/app/components/lamp-display.js
+++ LampProject/app/components/lamp-display.js
@@ -4,30 +4,55 @@ export default Ember.Component.extend({
 	metawearConnected: false,
 	macAddressOfBoard: 'DA:88:BB:64:7B:CB',
 	MWaccelHistory: [],
+	timeStacks: [],
+	activityPresent: 0,
+	getRequestSent: 0,
+	timeActivated: 0,
 	updateAccelData: function(component, result){
 		component.set('z', result.z);
-
+		var current_time = Date.now();
 		//update history, maintain 50 points max
 		var history=component.get('MWaccelHistory');
-
-		if(result.z <= 1.005 && result.z > 0.98){
+		var time_stacks = component.get('timeStacks');
+		var activity_present = component.get('activityPresent');
+		//Has to be set lower than normal(.976) because
+		//when the device gets warm, the noise increases
+		if(result.z <= 1.005 && result.z >= 0.976){
 			result.z = 1;
 		}
+		else{
+			for(var i=time_stacks.length-1; i>=0; i--){
+				if(current_time - time_stacks[i][0] <= 2000)
+				{
+					time_stacks[i].push(current_time);
+					if(time_stacks[i].length >= 30){
+						activity_present = 1;
+						component.set('activityPresent',  activity_present);
+						console.log('GOT IT!!!!');
+						time_stacks = [];
+						break;
+					}
+				}
+				else{
+					time_stacks.pop();
+				}
+			}
 
-		if(history.length === 150){
-			history.shiftObject();//shift an x off
-			//history.shiftObject();//shift a y off
-			//history.shiftObject();//shift a z off
+			time_stacks.unshift([current_time]);
 		}
-		var t = Date.now();
-		//var newXPoint = {time: t, label: 'x', value: result.x};
-		//var newYPoint = {time: t, label: 'y', value: result.y};
-		var newZPoint = {time: t, label: 'z', value: result.z};
+
+		if(history.length === 50){
+			history.shiftObject();//shift an x off
+		}
+		
+		var newZPoint = {time: current_time, label: 'z', value: result.z};
 		history.addObjects([newZPoint]);
-		//console.log('Added point: x=' + result.x + ', y='+result.y+', z='+result.z)
-		if(result.z != 1){
+		
+/*		if(result.z != 1){
 			console.log(newZPoint);
 		}
+*/
+
 	},
 	actions: {
 		accelON: function(component){
@@ -45,6 +70,58 @@ export default Ember.Component.extend({
 							alert('error: '+error);
 						}
 					);
+
+					setInterval(function(){
+						//the get request url after conditions have been met
+						var theUrl = "http://192.168.1.222:8000/";
+						
+						var get_request_sent = component.get('getRequestSent');
+						var activity_present = component.get('activityPresent');
+
+						//sends the on request if we see activity present
+						//and the on request has not been sent yet
+						if(get_request_sent == 0 && activity_present == 1){
+							var xmlHttp = new XMLHttpRequest();
+							//Not asynchronous because at this point
+							//our objective has been achieved if this sends
+							xmlHttp.open( "GET", theUrl+"on", false ); // false for synchronous request
+							xmlHttp.send( null );
+							console.log("On Request Sent to:" + theUrl + "on");
+							//can use this if we want it later
+							//var response_recieved = xmlHttp.responseText;	
+							get_request_sent = 1;
+							component.set("getRequestSent", get_request_sent);
+							component.set("timeActivated", Date.now());
+						}
+			
+						//sends the off request if 60 seconds have passed without activity
+						//otherwise, resets the counter
+						if(get_request_sent==1)
+						{
+							var current_time = Date.now();
+							var time_activated = component.get("timeActivated");
+							//if it has been 60 secs. since last activation
+							if(current_time - time_activated >= 60000){ 
+								var xmlHttp = new XMLHttpRequest();
+								// false for synchronous request
+								xmlHttp.open( "GET", theUrl+"off", false ); 
+								xmlHttp.send( null );
+								console.log("Off Request Sent to:" + theUrl + "off");
+								//can use this if we want it later
+								//var response_recieved = xmlHttp.responseText;
+								component.set("getRequestSent",0);
+							}
+							else{
+								if(activity_present == 1){
+									component.set("timeActivated", current_time);
+								}
+							}
+							
+							component.set("activityPresent", 0);
+						}
+
+
+					}, 1000);
 				}
 				catch(err){
 					console.log('error: '+err);
